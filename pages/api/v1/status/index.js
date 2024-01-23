8cb8eb5634f67b95f8f02ac44e01e53f6b755bb9
Djonathan Krause <email@djonathan.com>
2024-01-23 19:49:55 -0300

    endpoint /status returns db status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from 'infra/database';
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1+1;')
-  console.log(result)
-  response.status(200).json({ status: 'OK' });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionValue = dbMaxConnectionResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenConnectionsResult = await database.query({
+    text: "SELECT COUNT(1)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName]
+  });
+
+  const dbOpenConnectionsValue = dbOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionValue),
+        opened_connections: dbOpenConnectionsValue
+      }
+    }
+  });
 }
 
 export default status;
