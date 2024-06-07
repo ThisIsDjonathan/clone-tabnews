06c953e27f4b225fa40f1795e493c5621ce7b36c
Djonathan Krause <email@djonathan.com>
2024-06-07 17:14:22 -0300

    making the  more robust so we can run it on CI

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,10 @@
-import database from "infra/database";
+import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public;");
+});
 
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
@@ -12,18 +12,18 @@ test("POST to /api/v1/migrations should return 200", async () => {
   });
   expect(response1.status).toBe(201);
 
-  const responseBody1 = await response1.json();
+  const response1Body = await response1.json();
 
-  expect(Array.isArray(responseBody1)).toBe(true);
-  expect(responseBody1.length).toBeGreaterThan(0);
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
 
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
   expect(response2.status).toBe(200);
 
-  const responseBody2 = await response2.json();
+  const response2Body = await response2.json();
 
-  expect(Array.isArray(responseBody2)).toBe(true);
-  expect(responseBody2.length).toBe(0);
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
