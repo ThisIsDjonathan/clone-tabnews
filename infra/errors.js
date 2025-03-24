7e4160153deb33f289c368bea7f5804d6002505b
Djonathan Krause <email@djonathan.com>
2025-03-24 16:07:13 +0100

    feat: creates the NotFoundError

--- infra/errors.js
+++ infra/errors.js
@@ -76,3 +76,23 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "We could not find this", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action = action || "Check the params and try again.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
